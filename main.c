53abe720d656a763e8823b6491803f2402a95791
nagolove <dekar.own@gmail.com>
2023-10-30 16:02:16 +0300

    sinus generator attempt

--- main.c
+++ main.c
@@ -8,28 +8,22 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 
-unsigned int sample_rate = 44100;
-ALCdevice *openal_output_device;
-ALCcontext *openal_output_context;
-ALuint source;
+unsigned int    sample_rate = 44100;
+ALCdevice       *openal_output_device;
+ALCcontext      *openal_output_context;
+ALuint          source;
 
 int al_check_error(const char *given_label) {
-
     ALenum al_error;
     al_error = alGetError();
-
-    if (AL_NO_ERROR != al_error)
-    {
-
+    if (AL_NO_ERROR != al_error) {
         printf("ERROR - %s  (%s)\n", alGetString(al_error), given_label);
         return al_error;
     }
     return 0;
 }
 
-void MM_init_al()
-{
-
+void MM_init_al() {
     const char *defname = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
     printf("MM_init_al: defname %s\n", defname);
 
@@ -41,9 +35,7 @@ void MM_init_al()
     al_check_error("failed call to alGenBuffers");
 }
 
-void MM_exit_al()
-{
-
+void MM_exit_al() {
     // Stop the sources
     alSourceStopv(1, &source); //      streaming_source
     alSourcei(source, AL_BUFFER, 0); // зачем?
@@ -114,8 +106,7 @@ void play_buffer(short *buff, size_t buff_size) {
     }
 }
 
-static void list_audio_devices(const ALCchar *devices)
-{
+static void list_audio_devices(const ALCchar *devices) {
         const ALCchar *device = devices, *next = devices + 1;
         size_t len;
 
@@ -130,26 +121,74 @@ static void list_audio_devices(const ALCchar *devices)
         fprintf(stdout, "----------\n");
 }
 
+struct GeneratorSin {
+    double  amplitude;
+    double  duration;   // in seconds
+};
+
+struct GeneratorSquare {
+    double  amplitude;
+    double  duration;   // in seconds
+};
+
+typedef short int (*Generator)(double t, void *ctx);
+
+// 0 <= t <= duration
+static short int gen_sin(double t, void *ctx) {
+    assert(ctx);
+    struct GeneratorSin *g = ctx;
+    return sinf(t) * g->amplitude;
+}
+
+static short int gen_square(double t, void *ctx) {
+    assert(ctx);
+    struct GeneratorSquare *g = ctx;
+    return sinf(t) * g->amplitude;
+}
+
 static void MM_render_one_buffer() {
-    // float freq = 440.f;
+    // double freq = 440.f;
+
     // Время измеряется в дробных частях секунды
-    float seconds = 3.;
-    size_t buf_len = floor(seconds) * sample_rate;
+    double seconds = 3.;
+
+    // количество кусочков звука
+    size_t buf_len = floor(seconds) * sample_rate; 
+
     size_t buf_size = buf_len * sizeof(short int);
     short int *data = malloc(buf_size);
-    memset(data, 0, buf_size);
     assert(data);
+    memset(data, 0, buf_size);
 
     srand(time(NULL));
 
-    for (int i = 0; i < buf_len / 2; i += 2) {
-        data[i] += sin(i) * 2000.;
-    }
+    struct GeneratorSin gs = {
+        .amplitude = 10000.,
+        .duration = .1,
+    };
+
+    Generator gen = gen_sin;
 
     // Как записать треугольник?
     // Как записать квадрат?
     // Интерфейс записи волны
 
+    double duration = 0.;
+    //double dt = seconds / sample_rate;
+    double dt = 1. / sample_rate;
+    printf("dt %f\n", dt);
+    for (int i = 0; i < buf_len; i += 2) {
+        data[i] += gen(duration, &gs);
+        printf("data[%d] = %d\n", i, data[i]);
+        printf("duration %f\n", duration);
+        duration += dt;
+        if (duration >= gs.duration) {
+            printf("duration reset\n");
+            duration = 0.;
+        }
+    }
+
+    /*
     for (int i = 0; i < buf_len / 2; i += 2) {
         data[i] += sin(i + M_PI * 2) * 2000.;
     }
@@ -157,6 +196,7 @@ static void MM_render_one_buffer() {
     for (int i = buf_len / 2; i < buf_len; i += 2) {
         data[i] = rand() % 1000;
     }
+    */
 
     play_buffer(data, buf_size);
 
